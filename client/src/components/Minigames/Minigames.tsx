aa6d226e485737bfd5d17ef235764fe6de2b2048
CorcozLeonard <leonardcorcoz@yahoo.com>
2023-07-31 21:14:21 +0300

    minigames-photos

--- client/src/components/Minigames/Minigames.tsx
+++ client/src/components/Minigames/Minigames.tsx
@@ -1,16 +1,21 @@
 import Minigame from "./Minigame";
 import styles from "./Minigames.module.css";
-import img from "../../assets/bg1.png";
+import survival from "../../assets/minigames/survival.png";
+import skywars from "../../assets/minigames/skywars.png";
+import bedwars from "../../assets/minigames/bedwars.png";
+import oneblock from "../../assets/minigames/oneblock.png";
+import skyblock from "../../assets/minigames/skyblock.png";
+import tntrun from "../../assets/minigames/TNTrun.png";
 import { motion } from "framer-motion";
 
 const Minigames = () => {
   const minigames = [
-    { name: "Hunger Games", image: img },
-    { name: "Hunger Games", image: img },
-    { name: "Hunger Games", image: img },
-    { name: "Hunger Games", image: img },
-    { name: "Hunger Games", image: img },
-    { name: "Hunger Games", image: img },
+    { name: "Survival", image: survival },
+    { name: "Sky Wars", image: skywars },
+    { name: "Bed Wars", image: bedwars },
+    { name: "One Block", image: oneblock },
+    { name: "Sky Block", image: skyblock },
+    { name: "TNT Run", image: tntrun },
   ];
   return (
     <div className={styles.grid} id="minigames">
